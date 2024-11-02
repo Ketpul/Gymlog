a578a96457dc3aced8fd26d744ba770941b2fb7a
Ketpul <emiliqnnik07@abv.bg>
2024-11-02 15:12:10 +0200

    Update Nuget Packet

--- Gymlog.Infrastructure/Migrations/20241102130940_newmig.Designer.cs
+++ Gymlog.Infrastructure/Migrations/20241102130940_newmig.Designer.cs
@@ -4,6 +4,7 @@ using Gymlog.Infrastructure.Data.SeedDb;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Gymlog.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241102130940_newmig")]
+    partial class newmig
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
